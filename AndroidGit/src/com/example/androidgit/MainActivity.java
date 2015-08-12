e332257a4ef28d350daa3d82f5bd8cedbd59ab2f
petyr.zhan <petyr.zhan@dji.com>
2015-08-12 20:28:45 +0800

    9 01

--- AndroidGit/src/com/example/androidgit/MainActivity.java
+++ AndroidGit/src/com/example/androidgit/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Log.e("", "hello world 8");
+        Log.e("", "hello world 9");
     }
 }
