7a4fa7fa309b27a74c6f90100f12c0720638b6c0
petyr.zhan <petyr.zhan@dji.com>
2015-08-12 16:42:14 +0800

    修改文件2

--- AndroidGit/src/com/example/androidgit/MainActivity.java
+++ AndroidGit/src/com/example/androidgit/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Log.e("", "hello world");
+        Log.e("", "hello world 2");
     }
 }
